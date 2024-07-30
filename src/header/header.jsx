04f5934447e83de5f7b01429cb3479719873fc42
minospeed007 <nwannemino@gmail.com>
2024-07-30 19:17:39 +0000

    Update header.jsx

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -6,7 +6,7 @@ const Header =()=>{
    <div className="header-root-div">
     <div className="header-div">
     <div className="header-title-h1">
-            <h2 className='header-h1'>marketfeed</h2>
+            <h1 className='header-h1'>sarto</h1>
         </div>
         <div className="header-title">
             <h4 className='header-h4'>About Us</h4>
